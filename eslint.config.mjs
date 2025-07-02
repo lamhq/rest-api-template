9c37512786ac80dd3095c7579b0bc5ace36159ab
Lam <daibanglam@gmail.com>
2025-07-02 14:01:58 +0700

    chore: update packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'commitlint.config.mjs', '.husky/install.mjs'],
+    ignores: ['dist/', '**/*.mjs'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
